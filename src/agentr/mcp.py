74430dc32f07d45390f67f4b511ea8f1461e9cb2
Manoj Bajaj <manojbajaj95@gmail.com>
2025-03-27 20:33:14 +0530

    feat: Improvements to Api application

--- src/agentr/mcp.py
+++
@@ -1,7 +0,0 @@
-from agentr.server import AgentRServer
-
-mcp = AgentRServer(name="AgentR Server", description="AgentR Server", port=8005)
-
-
-if __name__ == "__main__":
-    mcp.run(transport="sse")
