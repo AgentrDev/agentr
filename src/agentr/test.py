1480774c816e3d70075ee17cba59acc2106a6a8b
Manoj Bajaj <manojbajaj95@gmail.com>
2025-03-27 16:07:00 +0530

    feat: Add agentr server

--- src/agentr/test.py
+++ src/agentr/test.py
@@ -1,35 +1,38 @@
-from agentr.server import TestServer
+from agentr.server import AgentRServer, LocalServer
 from agentr.store import MemoryStore
 
-store = MemoryStore()
-apps_list = [
-    {
-        "name": "tavily",
-        "integration": {
-            "name": "tavily_api_key",
-            "type": "api_key",
-            "store": {
-                "type": "environment",
-            }
-        },        
-    },
-    {
-        "name": "zenquotes",
-        "integration": None
-    },
-    {
-        "name": "github",
-        "integration": {
-            "name": "github",
-            "type": "agentr",
-        }
-    }
-]
-mcp = TestServer(name="Test Server", description="Test Server", apps_list=apps_list)
+# store = MemoryStore()
+# apps_list = [
+#     {
+#         "name": "tavily",
+#         "integration": {
+#             "name": "tavily_api_key",
+#             "type": "api_key",
+#             "store": {
+#                 "type": "environment",
+#             }
+#         },        
+#     },
+#     {
+#         "name": "zenquotes",
+#         "integration": None
+#     },
+#     {
+#         "name": "github",
+#         "integration": {
+#             "name": "github",
+#             "type": "agentr",
+#         }
+#     }
+# ]
+# mcp = LocalServer(name="Test Server", description="Test Server", apps_list=apps_list)
+
+mcp = AgentRServer(name="Test Server", description="Test Server", api_key="")
 
 async def test():
     tools = await mcp.list_tools()
-    print(tools)
+    from pprint import pprint
+    pprint(tools)
     result = await mcp.call_tool("star_repository", {"repo_full_name": "manojbajaj95/config"})
     print(result)
 
