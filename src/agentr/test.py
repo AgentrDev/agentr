a8303874eaa1531d4aa5932f4d9dc1b26dd04799
LoNeWoLf26548 <arstejas7@gmail.com>
2025-03-27 16:27:46 +0530

    removed changes from test and integration

--- src/agentr/test.py
+++ src/agentr/test.py
@@ -3,42 +3,35 @@ from agentr.store import MemoryStore
 
 store = MemoryStore()
 apps_list = [
-    # {
-    #     "name": "tavily",
-    #     "integration": {
-    #         "name": "tavily_api_key",
-    #         "type": "api_key",
-    #         "store": {
-    #             "type": "environment",
-    #         }
-    #     },        
-    # },
-    # {
-    #     "name": "zenquotes",
-    #     "integration": None
-    # },
-    # {
-    #     "name": "github",
-    #     "integration": {
-    #         "name": "github",
-    #         "type": "agentr",
-    #     }
-    # },
     {
-        "name": "reddit",
+        "name": "tavily",
         "integration": {
-            "name": "reddit",
-            "type": "agentr"
+            "name": "tavily_api_key",
+            "type": "api_key",
+            "store": {
+                "type": "environment",
+            }
+        },        
+    },
+    {
+        "name": "zenquotes",
+        "integration": None
+    },
+    {
+        "name": "github",
+        "integration": {
+            "name": "github",
+            "type": "agentr",
         }
-    }
+    },
 ]
 mcp = TestServer(name="Test Server", description="Test Server", apps_list=apps_list)
 
 async def test():
     tools = await mcp.list_tools()
     print(tools)
-    # result = await mcp.call_tool("star_repository", {"repo_full_name": "manojbajaj95/config"})
-    # print(result)
+    result = await mcp.call_tool("star_repository", {"repo_full_name": "manojbajaj95/config"})
+    print(result)
 
 if __name__ == "__main__":
     import asyncio
